2c712e885521c3b3062ae114562ba2b19b8319c0
Firebolt9907 <rishushar99@gmail.com>
2024-10-08 20:41:55 -0500

    created a test op for testing sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTest.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.hardware.rev.RevTouchSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
+@TeleOp(name = "Sensor Test", group = "TeleOp")
+public class SensorTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+//        BaseRobot baseRobot = new BaseRobot(hardwareMap, gamepad1, gamepad2, telemetry);
+        final RevTouchSensor touch;
+        final RevColorSensorV3 color;
+        touch = hardwareMap.get(RevTouchSensor.class, "touchSensor2");
+        color = hardwareMap.get(RevColorSensorV3.class, "colorSensor");
+        telemetry.setAutoClear(false);
+        Telemetry.Item isTouching = telemetry.addData("touch", false);
+        Telemetry.Item red = telemetry.addData("red", 0);
+        Telemetry.Item blue = telemetry.addData("blue", 0);
+        Telemetry.Item green = telemetry.addData("green", 0);
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            isTouching.setValue(touch.isPressed());
+            red.setValue(color.red());
+            green.setValue(color.green());
+            blue.setValue(color.blue());
+            telemetry.update();
+//            baseRobot.driveGamepads();
+
+//            if (Settings.Deploy.ODOMETRY) {
+//                baseRobot.odometry.update();
+//            }
+        }
+//        baseRobot.shutDown();
+    }
+
+}
